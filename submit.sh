55711d379b74757c7fb86191f03e4ab0cf7dbeb4
bill10 <bill@dhcp-v880-14-00691.1Xwireless.unc.edu>
2017-09-18 16:38:49 -0400

    script for submit to slurm

---
+++ submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=wikiparser
+#SBATCH --output=%A_%a.out
+#SBATCH --error=%A_%a.err
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=5:00:00
+#SBATCH --mem=4000
+
+# load python and packages
+module add python
+export PYTHONPATH=/nas/longleaf/home/bill10/Library/lib/python3/:$PYTHONPATH
+
+# grab out filename from the array 
+mapfile -t FILES < "all_dumps.txt"
+FILENAME=${FILES[$SLURM_ARRAY_TASK_ID]}
+
+# Copy files from Stash
+#wget http://stash.osgconnect.net/+bill10/Wiki/$FILENAME
+#7z e $FILENAME
+
+# Run python script
+python3 policy_parser.py $FILENAME
