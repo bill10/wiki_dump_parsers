adc66a25c443c02f3d6606c5c5a72fa9a073dbb7
bill10 <bill@Bills-MacBook-Pro.local>
2017-11-16 17:29:14 -0500

    remove stop words in ti-idf parser

--- tfidf_parser.py
+++ tfidf_parser.py
@@ -1,5 +1,10 @@
 # This parser will extract the following information for every page.
 from collections import Counter
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+stop_words= set(stopwords.words("english"))
 
 columns=['title','word','freq']
 
@@ -15,7 +20,8 @@ def parse(page):
         return res
     text=rev.text.lower()
     text=''.join([i if i.isalnum() else ' ' for i in text])
-    count=Counter(text.split())
+    text = [word for word in text.split() if word not in stop_words]
+    count=Counter(text)
     if len(count)==0:
         return res
     res=zip([page.title]*len(count),count.keys(),count.values())
