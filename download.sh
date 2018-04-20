a6b8deaebd28f850aa3abc4a5b6ba533298a80fb
bill10 <billanson10@gmail.com>
2018-04-20 19:16:13 -0400

    minor fix

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    wget wget https://dumps.wikimedia.org/enwiki/20180401/$line
+    wget https://dumps.wikimedia.org/enwiki/20180401/$line
     7z e $line
     rm $line
 done < "$1"
